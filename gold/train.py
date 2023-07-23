631f775dca2113d3f1436e8b69a8cff15e5a02ed
Vikas Jha <vikasjhanitk@gmail.com>
2023-07-23 13:03:54 +0000

    Updated gpt files

--- gold/train.py
+++ gold/train.py
@@ -1,9 +1,13 @@
 from typing import Tuple, Dict
-
+import mlflow, os
 import lightning as L
 import torch
 import hydra
 from omegaconf import DictConfig
+
+import mlflow.pytorch
+from mlflow import MlflowClient
+from lightning.pytorch.loggers.mlflow import MLFlowLogger
 
 from gold import utils
 
@@ -25,15 +29,27 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
     log.info(f"Instantiating model <{cfg.model._target_}>")
     model: L.LightningModule = hydra.utils.instantiate(cfg.model)
 
+    log.info("Instantiating loggers...")
+    logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
+
+    log.info("Instantiating callbacks...")
+    callbacks: List[Callback] = utils.instantiate_callbacks(cfg.get("callbacks"))
+
     log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
-    trainer: L.Trainer = hydra.utils.instantiate(cfg.trainer)
+    trainer: L.Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
 
     object_dict = {
         "cfg": cfg,
         "datamodule": datamodule,
         "model": model,
         "trainer": trainer,
+        "callbacks": callbacks,
+        "logger": logger,
     }
+
+    if logger:
+        log.info("Logging hyperparameters!")
+        utils.log_hyperparameters(object_dict)
 
     if cfg.get("compile"):
         model = torch.compile(model)
@@ -55,9 +71,9 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
     log.info("Scripting Model ...")
 
     scripted_model = model.to_torchscript(method="script")
-    torch.jit.save(scripted_model, f"{cfg.paths.output_dir}/model.script.pt")
+    torch.jit.save(scripted_model, f"{cfg.model_ckpt_path}")
 
-    log.info(f"Saving traced model to {cfg.paths.output_dir}/model.script.pt")
+    log.info(f"Saving traced model to {cfg.model_ckpt_path}")
 
 
     if cfg.get("test"):
@@ -68,6 +84,18 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
             ckpt_path = None
         trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
         log.info(f"Best ckpt path: {ckpt_path}")
+
+        for logger_ in logger:
+            if isinstance(logger_, MLFlowLogger):
+                ckpt = torch.load(ckpt_path)
+                model.load_state_dict(ckpt["state_dict"])
+                os.environ['MLFLOW_RUN_ID'] = logger_.run_id
+                os.environ['MLFLOW_EXPERIMENT_ID'] = logger_.experiment_id
+                os.environ['MLFLOW_EXPERIMENT_NAME'] = logger_._experiment_name
+                os.environ['MLFLOW_TRACKING_URI'] = logger_._tracking_uri
+                mlflow.pytorch.log_model(model, "model")
+                break
+
 
     test_metrics = trainer.callback_metrics
 
--- gold/utils/__init__.py
+++ gold/utils/__init__.py
@@ -1,2 +1,5 @@
 from gold.utils.pylogger import get_pylogger
-from gold.utils.utils import task_wrapper, get_metric_value
+from gold.utils.utils import task_wrapper, get_metric_value, extras
+from gold.utils.instantiators import instantiate_loggers, instantiate_callbacks
+from gold.utils.logging_utils import log_hyperparameters
+from gold.utils.rich_utils import enforce_tags, print_config_tree
